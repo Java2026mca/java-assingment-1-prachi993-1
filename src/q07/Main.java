a9a203ee6c90ffbb5b81735f140aefc55d17f73c
prachi <palprachiiiii025@gmail.com>
2026-04-08 15:03:19 +0530

    Update Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,41 +1,38 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class BubbleSortWithSwaps {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        // Input array
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
 
         int swaps = 0;
 
         // Bubble Sort
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j + 1]){
                     // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-
                     swaps++;
                 }
             }
         }
 
         // Print sorted array
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             System.out.print(arr[i]);
-            if (i < n - 1) {
-                System.out.print(" ");
-            }
+            if(i < n - 1) System.out.print(" ");
         }
 
+        // Print swaps
         System.out.println();
         System.out.println("Swaps: " + swaps);
     }
